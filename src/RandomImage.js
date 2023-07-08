d0753a4efeb5912c812f38cada9048eb5a85c2fc
Tarun Sharma <44375597+tarunshrm786@users.noreply.github.com>
2023-07-08 15:34:16 +0530

    third  commit

--- src/RandomImage.js
+++ src/RandomImage.js
@@ -1,39 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-// import './App.css'; // Import the CSS file here
-
-// const RandomImage = () => {
-//   const [imageURL, setImageURL] = useState('');
-//   const [isLoading, setIsLoading] = useState(true);
-
-//   useEffect(() => {
-//     fetchRandomImage();
-//   }, []);
-
-//   const fetchRandomImage = async () => {
-//     try {
-//       const response = await axios.get('https://picsum.photos/500');
-//       setImageURL(response.request.responseURL);
-//       console.log('Image URL---------:', imageURL);
-//       setIsLoading(false);
-//     } catch (error) {
-//       console.error('Error fetching random image:', error);
-//     }
-//   };
-
-//   return (
-//     <div className="random-image-container">
-//       {isLoading ? (
-//         <div className="loader"></div>
-//       ) : (
-//         <img className="random-image" src={imageURL} alt="Random" />
-//       )}
-//     </div>
-//   );
-// };
-
-// export default RandomImage;
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { FacebookShareButton, TwitterShareButton, WhatsappShareButton } from 'react-share';
